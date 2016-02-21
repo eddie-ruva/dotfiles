e343aa0420d252d48d81851386c3b86baa89e4c2
Edilberto Ruvalcaba <eddie.ruva@gmail.com>
2016-02-21 14:01:46 -0800

    Install vim Vundle as part of setup and install all plugins

--- setup.sh
+++ setup.sh
@@ -33,3 +33,9 @@ do
     ln -s $full_path ~/.$file_name
   fi
 done
+
+if [ ! -d "/Users/edilberto/.vim/bundle/Vundle.vim" ]
+then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+vim +PluginInstall +qall
